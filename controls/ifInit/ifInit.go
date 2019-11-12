5968b50156b68e5fa377aab19a23a05bc8abb555
VAGNER NASCIMENTO <x309358@gruposantander.com>
2019-11-12 12:19:44 -0300

    Controls - if with var initialization

---
+++ controls/ifInit/ifInit.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func main() {
+	/*
+		before ";" the variable i is created and initialized with a value
+		this variable is visible only inside if or else or "else if" of the same sentence
+	*/
+	if i := aletoryNumber(); i > 5 { // it works also to switch
+		fmt.Println("Win, i:", i)
+	} else {
+		fmt.Println("Lose, i:", i)
+	}
+
+	//fmt.Println(i) // the variable "i" doesn't exists out of if sentence
+}
+
+func aletoryNumber() int {
+	s := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(s)
+
+	return r.Intn(10)
+}
