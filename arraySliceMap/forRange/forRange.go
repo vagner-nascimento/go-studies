0e191eef91e8274affeb3ee858500ae246287c1f
VAGNER NASCIMENTO <x309357@gruposantander.com>
2019-11-13 10:57:48 -0300

    Using for with RANGE to use like forEach structure

---
+++ arraySliceMap/forRange/forRange.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	// declaring an array with inference, it uses the number of given elements to define the size
+	numbers := [...]int{1, 2, 3, 4, 5}
+
+	/*
+		in this way, for takes the index and the value at this index
+		looks like .every() of JS (ECMA6)
+	*/
+	for index, value := range numbers {
+		fmt.Printf("index %d, value %d\n", index, value)
+	}
+
+	// ignoring index, looks like .forEach()
+	for _, value := range numbers {
+		fmt.Println(value)
+	}
+}
