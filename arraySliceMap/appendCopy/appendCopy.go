5ba73c6f4a9717f594be6e6e3f4a86f7d1e49a70
Vagner Nascimento <vagnernasc88@gmail.com>
2019-11-25 22:30:08 -0300

    Slices appen() and copy()

---
+++ arraySliceMap/appendCopy/appendCopy.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	array1 := [3]int{1, 2, 3}
+	var slice1 []int
+
+	// array1 = append(array1, 1, 2, 3) //append only works with slices
+	slice1 = append(slice1, 4, 5, 6)
+	fmt.Println(array1, slice1)
+
+	// slice1 = append(slice1, array1) //it doesn't works too
+	slice2 := make([]int, 2)
+	copy(slice2, slice1) //the slice2 has only 2 positions, so it copy only the first 2 elements of the slice2, which has len 3
+	fmt.Println(slice1, slice2)
+
+	slice3 := make([]int, 3)
+	copy(slice3, slice2) //it copy only the 2 first elements into the slice3
+	fmt.Println(slice2, slice3)
+}
