a020baf156c98c52d8168d7c5339e025229376c1
VAGNER NASCIMENTO <x309357@gruposantander.com>
2019-12-30 22:23:29 -0300

    Packages - Creating a reusable package

---
+++ package/reuse/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/vagner-nascimento/area"
+)
+
+func main() {
+	fmt.Println(area.Circ(6.0))
+	fmt.Println(area.Rect(5.0, 2.0))
+	fmt.Println(area.Pi)
+	// fmt.Println(area._EqTriangle(5.0, 2.0)) // It doesn't works because the function _EqTriangle is private on the area package
+}
