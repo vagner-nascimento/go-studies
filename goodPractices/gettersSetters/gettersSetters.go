9618389cef8d9b947ce84f74cb1e4ed85edc2c2b
VAGNER NASCIMENTO <x309357@gruposantander.com>
2020-02-07 01:39:39 -0300

    Good Practices - Getter and Setters convetion names

--- goodPractices/gettersSetters/gettersSetters.go
+++ goodPractices/gettersSetters/gettersSetters.go
@@ -1,1 +1,31 @@
-package goodPractices
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type customer struct {
+	Name string
+}
+
+type order struct {
+	c customer
+}
+
+// Getters should have only the type name
+func (o *order) Customer() customer {
+	return o.c
+}
+
+func (o *order) SetCustomer(c customer)  {
+	o.c = c
+}
+
+func main() {
+	o := order{}
+	o.SetCustomer(customer{Name:"Gerald"})
+
+	b, _ := json.Marshal(o.Customer())
+	fmt.Printf("Customer from getter %s\n", string(b))
+}
