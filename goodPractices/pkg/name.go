d68922d5cbad49197e0e2f1723bf0c22c606a0c0
VAGNER NASCIMENTO <x309357@gruposantander.com>
2020-02-07 00:56:38 -0300

    Good Practices - Pakage names

---
+++ goodPractices/pkg/name.go
@@ -0,0 +1,8 @@
+package pkg
+
+/*
+	- Packages should have the same name to the folder
+	- Packages name should be short and with only lower case chars.
+	- Packages name should not have numbers, symbols, underscore ( _ ) nor hyphen ( - )
+	- A good name is, as bellow, package
+*/
